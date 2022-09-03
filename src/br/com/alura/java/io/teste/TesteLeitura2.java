7d0838fc1714b7daeb1795b7c77007a06cf1d9d8
Paulo Ferreira <phgferreira@outlook.com>
2022-09-03 15:17:53 -0300

    Usando um linhaScanner com delimitador

--- src/br/com/alura/java/io/teste/TesteLeitura2.java
+++ src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -16,8 +16,21 @@ public class TesteLeitura2 {
 			String linha = scanner.nextLine();
 			System.out.println(linha);
 			
-			String valores[] = linha.split(",");
-			System.out.println(Arrays.toString(valores));
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useDelimiter(",");
+			
+			String valor1 = linhaScanner.next();
+			String valor2 = linhaScanner.next();
+			String valor3 = linhaScanner.next();
+			String valor4 = linhaScanner.next();
+			String valor5 = linhaScanner.next();
+
+			System.out.println(valor1 + ", " + valor2 + ", " + valor3 + ", " + valor4 + ", " + valor5);
+			
+			linhaScanner.close();
+			
+//			String valores[] = linha.split(",");
+//			System.out.println(Arrays.toString(valores));
 		}
 		
 		scanner.close();

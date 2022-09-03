8d61d80d50a827782f454f7131d22446d86a9076
Paulo Ferreira <phgferreira@outlook.com>
2022-09-03 20:12:45 -0300

    Testando valor default de variável de tipo primitivo e wrapper

---
+++ src/br/com/alura/java/io/teste/EntidadeTeste.java
@@ -0,0 +1,8 @@
+package br.com.alura.java.io.teste;
+
+public class EntidadeTeste {
+
+	public double valor;
+	
+	public Double valor2;
+}
---
+++ src/br/com/alura/java/io/teste/TesteValorDefaultWrapper.java
@@ -0,0 +1,12 @@
+package br.com.alura.java.io.teste;
+
+public class TesteValorDefaultWrapper {
+
+	public static void main(String[] args) {
+		EntidadeTeste entidadeTeste = new EntidadeTeste();
+		System.out.println(entidadeTeste.valor);
+		System.out.println(entidadeTeste.valor2);
+	}
+
+}
+
